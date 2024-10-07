1db1590bea373339f0bac2419c2d919e1010dce1
Erick Ochieng <erickochieng830@gmail.com>
2024-10-07 11:02:54 +0300

    made deployment changes

--- records_management/records_storage/records/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Department, Record, User
-
-admin.site.register(Department)
-admin.site.register(Record)
-admin.site.register(User)
